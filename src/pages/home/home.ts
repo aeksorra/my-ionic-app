6529e03b539418b13c270160a4547b9f4b607be6
aeksorra <gsorra@gmail.com>
2018-05-10 10:07:55 +0700

    แก้ไขตาม อ.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  vessels=[];
+  vesselName;
 
+  constructor(public navCtrl: NavController public http: HttpClient) {
+
+  }
+
+    public loadData(vesselName){
+    // this.vessels.push(vesselName);
+    // this.vesselName="";
+    
+    let url = "http://localhost/app/index.php/api/FishingVessel.php/allship"
+    
+    this.http.get(url)
+             .subscribe(
+               (result:any)=>{
+                 console.log(result);
+                 this.vessels=result;
+               }
+             );
   }
 
 }
